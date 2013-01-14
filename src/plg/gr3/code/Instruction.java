4baf40988b85a905377194f9c40d7386d0abf8ed
Arturo Pareja <arturo.pareja.garcia@gmail.com>
2013-01-14 17:03:02 +0100

    Creada clase abstracta Instruction

--- src/plg/gr3/code/Instruction.java
+++ src/plg/gr3/code/Instruction.java
@@ -1,5 +1,17 @@
 package plg.gr3.code;
 
+/**
+ * Clase abstracta de la que heredan las instrucciones de la maquina pila
+ * 
+ * @author PLg Grupo 03 2012/2013
+ */
+
 public abstract class Instruction {
     
+    /**
+     * Constructor privado a nivel de paquete
+     * */
+    protected Instruction () {
+        
+    }
 }
