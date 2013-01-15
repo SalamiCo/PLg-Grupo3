54cb6d012c774c9507ac5e38f017923af21da97d
Daniel Escoz <darkhogg@gmail.com>
2013-01-15 17:14:39 +0100

    Documentada BinaryOperator y añadido `compareNumbers`

--- src/plg/gr3/BinaryOperator.java
+++ src/plg/gr3/BinaryOperator.java
@@ -2,7 +2,13 @@ package plg.gr3;
 
 import plg.gr3.parser.Type;
 
+/**
+ * Clase que representa los operadores binarios del lenguaje.
+ * 
+ * @author PLg Grupo 03 2012/2013
+ */
 public enum BinaryOperator {
+    /** Operador de suma */
     ADDITION("+") {
         
         @Override
@@ -39,6 +45,7 @@ public enum BinaryOperator {
         }
         
     },
+    /** Operador de resta */
     SUBTRACTION("-") {
         @Override
         public boolean canApply (Type t1, Type t2) {
@@ -73,6 +80,8 @@ public enum BinaryOperator {
             }
         }
     },
+    
+    /** Operador de multiplicación */
     PRODUCT("*") {
         @Override
         public boolean canApply (Type t1, Type t2) {
@@ -107,6 +116,8 @@ public enum BinaryOperator {
             }
         }
     },
+    
+    /** Operador de división */
     DIVISION("/") {
         @Override
         public boolean canApply (Type t1, Type t2) {
@@ -141,7 +152,9 @@ public enum BinaryOperator {
             }
         }
     },
-    MODULE("%") {
+    
+    /** Operador de módulo */
+    MODULO("%") {
         @Override
         public boolean canApply (Type t1, Type t2) {
             return t1.isNumeric() && t2.isNumeric();
@@ -175,6 +188,8 @@ public enum BinaryOperator {
             }
         }
     },
+    
+    /** Operador de igualdad */
     EQUALS("==") {
         @Override
         public boolean canApply (Type t1, Type t2) {
@@ -198,6 +213,8 @@ public enum BinaryOperator {
             return Boolean.valueOf(o1.equals(o2));
         }
     },
+    
+    /** Operador de desigualdad */
     NOT_EQUALS("!=") {
         @Override
         public boolean canApply (Type t1, Type t2) {
@@ -221,6 +238,8 @@ public enum BinaryOperator {
             return !(Boolean.valueOf(o1.equals(o2)));
         }
     },
+    
+    /** Operación menor */
     LOWER_THAN("<") {
         @Override
         public boolean canApply (Type t1, Type t2) {
@@ -257,6 +276,8 @@ public enum BinaryOperator {
             }
         }
     },
+    
+    /** Operador menor o igual */
     LOWER_EQUAL("<=") {
         @Override
         public boolean canApply (Type t1, Type t2) {
@@ -293,6 +314,8 @@ public enum BinaryOperator {
             }
         }
     },
+    
+    /** Operador mayor */
     GREATER_THAN(">") {
         @Override
         public boolean canApply (Type t1, Type t2) {
@@ -329,6 +352,8 @@ public enum BinaryOperator {
             }
         }
     },
+    
+    /** Operador mayor o igual */
     GREATER_EQUALS(">=") {
         @Override
         public boolean canApply (Type t1, Type t2) {
@@ -365,6 +390,8 @@ public enum BinaryOperator {
             }
         }
     },
+    
+    /** Operación de y lógico */
     AND("and") {
         @Override
         public boolean canApply (Type t1, Type t2) {
@@ -390,6 +417,8 @@ public enum BinaryOperator {
             return b1 && b2;
         }
     },
+    
+    /** Operación o lógico */
     OR("or") {
         @Override
         public boolean canApply (Type t1, Type t2) {
@@ -416,14 +445,35 @@ public enum BinaryOperator {
         }
     };
     
+    /** Símbolo del operador */
     private final String symbol;
+    
+    /**
+     * Compara dos objetos de tipo {@link Number} entre sí.
+     * 
+     * @param n1
+     *            Primer número a comparar
+     * @param n2
+     *            Segundo número a comparar
+     * @return Entero positivo, cero o negativo dependiendo de si <tt>n1</tt> es mayor, igual o menor, respectivamente,
+     *         que <tt>n2</tt>.
+     */
+    private static int compareNumbers (Number n1, Number n2) {
+        if (n1 instanceof Float || n2 instanceof Float) {
+            return n1.floatValue() < n2.floatValue() ? -1 : n1.floatValue() > n2.floatValue() ? +1 : 0;
+            
+        } else if (n1 instanceof Integer || n2 instanceof Integer) {
+            return n1.intValue() - n2.intValue();
+            
+        } else if (n1 instanceof Natural || n2 instanceof Natural) {
+            return n1.intValue() - n2.intValue();
+        }
+        
+        throw new IllegalArgumentException();
+    }
     
     private BinaryOperator (String symbol) {
         this.symbol = symbol;
-    }
-    
-    private int compareNumbers (Number o1, Number o2) {
-        return 0;
     }
     
     @Override
@@ -431,9 +481,40 @@ public enum BinaryOperator {
         return this.symbol;
     }
     
+    /**
+     * Comprueba si este operador puede aplicarse para los tipos dados.
+     * 
+     * @param t1
+     *            Tipo del primer operando
+     * @param t2
+     *            Tipo del segundo operando
+     * @return <tt>true</tt> si este operador puede aplicarse a valores de tipo <tt>t1</tt> y <tt>t2</tt>,
+     *         <tt>false</tt>
+     */
     public abstract boolean canApply (Type t1, Type t2);
     
+    /**
+     * Devuelve el tipo resultado de aplicar este operador con los tipos dados, o un tipo error si no pueden aplicarse.
+     * 
+     * @param t1
+     *            Tipo del primer operando
+     * @param t2
+     *            Tipo del segundo operando
+     * @return El tipo que devolvería este operador al aplicarse sobre dos operandos de tipo <tt>t1</tt> y <tt>t2</tt>
+     */
     public abstract Type getApplyType (Type t1, Type t2);
     
+    /**
+     * Aplica el operador sobre los argumentos dados y devuelve el resultado. Si el operador no es aplicable a los
+     * argumentos dados, este método lanza una {@link IllegalArgumentException}.
+     * 
+     * @param o1
+     *            Primer operando
+     * @param o2
+     *            Segundo operando
+     * @return El valor resultado de aplicar el operador a los argumentos <tt>o1</tt> y <tt>o2</tt>
+     * @throws IllegalArgumentException
+     *             si los argumentos no se pueden operar con este operador
+     */
     public abstract Object apply (Object o1, Object o2);
 }
