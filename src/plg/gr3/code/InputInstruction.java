5b324325f6ba5b10b57daf07fa72f8e1f9cdd7e8
Daniel Escoz <darkhogg@gmail.com>
2013-01-15 10:54:16 +0100

    Renombrada la clase de la instrucción `in` y añadido un getter

---
+++ src/plg/gr3/code/InputInstruction.java
@@ -0,0 +1,27 @@
+package plg.gr3.code;
+
+import plg.gr3.parser.Type;
+
+/**
+ * Clase que implementa la instruccion in(type).
+ * 
+ * @author PLg Grupo 03 2012/2013
+ */
+public class InputInstruction extends Instruction {
+    
+    /** Tipo de la variable a la que se asigna el valor del buffer de entrada */
+    private Type inputType;
+    
+    /**
+     * @param inputType
+     *            Tipo del valor que se leerá
+     */
+    public InputInstruction (Type inputType) {
+        this.inputType = inputType;
+    }
+    
+    /** @return Tipo del valor que se leerá */
+    public Type getInputType () {
+        return inputType;
+    }
+}
