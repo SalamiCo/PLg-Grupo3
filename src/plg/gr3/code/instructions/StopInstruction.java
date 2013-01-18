7a7b2ae32d78eac8fa4f724ac6e0e27394e6a911
Arturo Pareja <arturo.pareja.garcia@gmail.com>
2013-01-18 15:50:51 +0100

    creada la instruccion de stop

---
+++ src/plg/gr3/code/instructions/StopInstruction.java
@@ -0,0 +1,17 @@
+package plg.gr3.code.instructions;
+
+import plg.gr3.vm.VirtualMachine;
+
+/**
+ * Clase que implementa la instruccion <tt>stop</tt>.
+ * 
+ * @author PLg Grupo 03 2012/2013
+ */
+public class StopInstruction extends Instruction {
+    
+    @Override
+    public void execute (VirtualMachine vm) {
+        vm.stop();
+    }
+    
+}
