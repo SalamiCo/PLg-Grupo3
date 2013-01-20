18386fce0ae1f6470716149321b091ea0598ac13
Daniel Escoz <darkhogg@gmail.com>
2013-01-20 18:42:48 +0100

    Vaciado Util

--- src/plg/gr3/Util.java
+++ src/plg/gr3/Util.java
@@ -2,32 +2,7 @@ package plg.gr3;
 
 public final class Util {
     
-    public static Object stringToNatural (String str) {
-        try {
-            int i = Integer.parseInt(str);
-            if (i < 0) {
-                return null;
-            }
-            
-            return i;
-            
-        } catch (NumberFormatException exc) {
-            return null;
-        }
+    public static void main (String[] args) {
+        
     }
-    
-    public static Object stringToFloat (String str) {
-        try {
-            float f = Float.parseFloat(str);
-            if (f < 0) {
-                return null;
-            }
-            
-            return f;
-            
-        } catch (NumberFormatException exc) {
-            return null;
-        }
-    }
-    
 }
