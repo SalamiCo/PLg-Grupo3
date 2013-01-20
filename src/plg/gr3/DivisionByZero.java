30320cf809ba0ce438429ae6f447c90c6bb8bc67
Antonio Irizar <antonioirizar@gmail.com>
2013-01-20 17:20:21 +0100

    creacion de los errores division por cero, pila vacia, natural negativo, memoria sin inicializar

---
+++ src/plg/gr3/DivisionByZero.java
@@ -0,0 +1,16 @@
+package plg.gr3;
+
+import plg.gr3.code.instructions.Instruction;
+
+public class DivisionByZero extends RuntimeError {
+    
+    public DivisionByZero (int position, Instruction instruction) {
+        super(position, instruction);
+    }
+    
+    @Override
+    public String getErrorMessage () {
+        return "Division por cero";
+    }
+    
+}
