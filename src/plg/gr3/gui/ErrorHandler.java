3a2bef1cb76dbb7bc6ef7a5641f4aa41a29a160a
Raul Marcos <raulmarcosl@gmail.com>
2013-02-12 16:46:31 +0100

    Console, Error y Problem Handlers

---
+++ src/plg/gr3/gui/ErrorHandler.java
@@ -0,0 +1,40 @@
+package plg.gr3.gui;
+
+import javax.swing.JTextPane;
+
+public class ErrorHandler {
+    
+    /**
+     * Tipos de mensaje posibles para los errores de tiempo de ejecución
+     * */
+    public enum ErrorType {
+        
+    }
+    
+    /**
+     * Área de texto que representa los errores de tiempo de ejecución
+     * */
+    private final JTextPane errorPane;
+    
+    /**
+     * Crea un nuevo área de texto (Error) de sólo lectura.
+     * */
+    public ErrorHandler () {
+        errorPane = new JTextPane();
+        errorPane.setEditable(false);
+    }
+    
+    /**
+     * Obtiene el área de texto que representa los errores de tiempo de ejecución
+     * */
+    public JTextPane getErrorPane () {
+        return errorPane;
+    }
+    
+    /**
+     * Inserta un nuevo mensaje del tipo especificado en el area de texto que representa los erroes.
+     * */
+    public void printError (ErrorType type, String msg) {
+        errorPane.setText(errorPane.getText() + "[" + type.toString() + "] " + msg + "\n");
+    }
+}
