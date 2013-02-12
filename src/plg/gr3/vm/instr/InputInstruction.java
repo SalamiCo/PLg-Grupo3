71423aa84fbbca20f142bab8543da3dd16cd8bda
Daniel Escoz <darkhogg@gmail.com>
2013-02-12 17:50:14 +0100

    Arreglado error con InputInstruction

--- src/plg/gr3/vm/instr/InputInstruction.java
+++ src/plg/gr3/vm/instr/InputInstruction.java
@@ -35,7 +35,7 @@ public class InputInstruction extends Instruction {
     public void execute (VirtualMachine vm) {
         try {
             // FIXME Qué
-            vm.pushValue(vm.getInput()); // Cima <= BufferIN
+            vm.pushValue(vm.getInput(inputType)); // Cima <= BufferIN
         } catch (IOException e) {
             // TODO Error en tiempo de ejecución: error al leer de BufferIN
             vm.abort(new IOError(vm.getProgramCounter(), this));
