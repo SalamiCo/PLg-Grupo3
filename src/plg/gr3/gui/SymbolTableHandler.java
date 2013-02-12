aafc746186e8a94b52a459ae6a8e56f3d1a61c3b
Raul Marcos <raulmarcosl@gmail.com>
2013-02-12 20:00:15 +0100

    Not working version of fillTable(st)

--- src/plg/gr3/gui/SymbolTableHandler.java
+++ src/plg/gr3/gui/SymbolTableHandler.java
@@ -1,7 +1,12 @@
 package plg.gr3.gui;
 
+import java.util.Iterator;
+import java.util.Set;
+
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
+
+import plg.gr3.parser.SymbolTable;
 
 /**
  * Clase que crea un manejador para el JTable que alberga la tabla de símbolos de la aplicación.
@@ -11,13 +16,13 @@ public class SymbolTableHandler {
     /**
      * Tabla que representa la tabla de símbolos.
      * */
-    private JTable symbolTable;
+    private final JTable symbolTable;
     
     /**
      * Crea una nueva tabla de símbolos de solo lectura.
      * */
-    public SymbolTableHandler () {
-        symbolTable = new JTable();
+    public SymbolTableHandler (SymbolTable st) {
+        this.symbolTable = new JTable();
         DefaultTableModel tableModel = new DefaultTableModel() {
             private static final long serialVersionUID = 4482796276781353831L;
             
@@ -28,7 +33,7 @@ public class SymbolTableHandler {
         };
         
         // auto resize off
-        symbolTable.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+        this.symbolTable.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
         // setting the column name
         String[] tableColumnNames = new String[5];
         tableColumnNames[0] = "Nombre";
@@ -36,18 +41,13 @@ public class SymbolTableHandler {
         tableColumnNames[2] = "Const";
         tableColumnNames[3] = "Valor";
         tableColumnNames[4] = "Dirección";
-        String[] tableColumnNames1 = new String[5];
-        tableColumnNames1[0] = "Nombre";
-        tableColumnNames1[1] = "Tipo";
-        tableColumnNames1[2] = "Const";
-        tableColumnNames1[3] = "Valor";
-        tableColumnNames1[4] = "sjdioañpinasoihdnasoihdasoihoidsahoash";
+        
         tableModel.setColumnIdentifiers(tableColumnNames);
         
-        tableModel.addRow(tableColumnNames1);
+        fillTable(st);
         
-        symbolTable.setModel(tableModel);
-        symbolTable.setVisible(true);
+        this.symbolTable.setModel(tableModel);
+        this.symbolTable.setVisible(true);
     }
     
     /**
@@ -55,5 +55,19 @@ public class SymbolTableHandler {
      * */
     public JTable getSymbolTable () {
         return symbolTable;
+    }
+    
+    private void fillTable (SymbolTable st) {
+        Set<String> keySet = st.getTable().keySet();
+        
+        String[] tableEntry = new String[5];
+        String nextEntry;
+        Iterator<String> it = keySet.iterator();
+        while (it.hasNext()) {
+            nextEntry = it.next();
+            tableEntry[0] = nextEntry;
+            // hay que meter en tableEntry[1], 2, etc, los valores de entry
+            Entry aaa = st.getTable().get(nextEntry);
+        }
     }
 }
