f13d86e375124477ce992cec8edd8cbb8ded7d5b
Pedro Morgado <p.morgado107@gmail.com>
2013-06-08 16:34:24 +0200

    Arreglado fallo en Attribution

--- src/plg/gr3/parser/Attribution.java
+++ src/plg/gr3/parser/Attribution.java
@@ -717,15 +717,20 @@ public final class Attribution extends Atribucion {
             public Object eval (Atributo... args) {
                 SymbolTable table = (SymbolTable) args[0].valor();
                 Lexeme ident = (Lexeme) args[1].valor();
+
+                List<CompileError> errs = new ArrayList<>();
+
                 if (table.hasIdentifier(ident.getLexeme())) {
-                    return new UndefinedIdentifierError(ident.getLexeme(), ident.getLine(), ident.getColumn());
+                    errs.add(new UndefinedIdentifierError(ident.getLexeme(), ident.getLine(), ident.getColumn()));
+
+                    ClassDec cd = table.getIdentfierClassDec(ident.getLexeme());
+                    if (cd != ClassDec.TYPE) {
+                        errs.add(new BadIdentifierClassError(
+                            ident.getLexeme(), cd, ClassDec.TYPE, ident.getLine(), ident.getColumn()));
+                    }
                 }
-                ClassDec cd = table.getIdentfierClassDec(ident.getLexeme());
-                if (cd != ClassDec.TYPE) {
-                    return (new BadIdentifierClassError(ident.getLexeme(), cd, ClassDec.TYPE, ident.getLine(), ident
-                        .getColumn()));
-                }
-                return null;
+
+                return errs;
             }
         });
 
@@ -2912,7 +2917,7 @@ public final class Attribution extends Atribucion {
 
     public TAtributos paren_R2 (TAtributos lit) {
         regla("Paren -> Lit");
-        TAtributos attr = atributosPara("Paren", "tsh", "tipo", "desig", "cod", "etqh", "etq", "err");
+        TAtributos attr = atributosPara("Paren", "tsh", "tipo", "desig", "cod", "etqh", "etq", "err", "valor");
 
         asigna(attr.a("desig"), a(false));
 
